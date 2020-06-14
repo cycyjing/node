a3f75a60b03726c062b0c0c41145da18ecfebe01
cycyjing <cycycyjing@gmail.com>
2020-06-15 11:48:26 +1200

    feat(express): get and post methods

--- node-demo03-express/index.js
+++ node-demo03-express/index.js
@@ -1,16 +1,46 @@
 const express = require('express')
 const app = express()
-/* params */
+const bodyParser = require('body-parser')
+/** params */
 // app.get('/:id', (req, res) => {
 //   console.log(req.params)
 //   console.log('get method')
 //   res.send('hello world')
 // })
+
 /** Static files: http://localhost:8081/public/yang.png */
-app.use('/public', express.static('public'))
-app.get('/', (req, res) => {
-  res.send('Static静态')
+// app.use('/public', express.static('public'))
+// app.get('/', (req, res) => {
+//   res.send('Static静态')
+// })
+
+/** GET method */
+// app.get('/index.html', (req, res) => {
+//   res.sendFile(__dirname + "/index.html")
+// })
+// app.get('/process-get', (req, res) => {
+//   const response = { firstname: req.query.firstname, lastname: req.query.lastname }
+//   console.log(response)
+//   res.end(JSON.stringify(response))
+// })
+
+/** POST Method */
+// app.use(bodyParser.urlencoded({ extended: false }))
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.get('/index.html', (req, res) => {
+  res.sendFile(__dirname + "/index.html")
 })
+app.post('/process-post', urlencodedParser, (req, res) => {
+  const response = { firstname: req.body.firstname, lastname: req.body.lastname }
+    console.log(response)
+    res.end(JSON.stringify(response))
+})
+
+
+
+
+
+
 const server = app.listen(8081, () => {
   const host = server.address().address
   const port = server.address().port
