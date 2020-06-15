4b8657985eeb95b9046376ce65e265f522142af4
cycyjing <cycycyjing@gmail.com>
2020-06-15 18:00:50 +1200

    feat(express): demo03 image upload

--- node-demo03-express/index.js
+++ node-demo03-express/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const fs = require('fs')
+const multer = require('multer')
+const { response } = require('express')
 /** params */
 // app.get('/:id', (req, res) => {
 //   console.log(req.params)
@@ -25,15 +28,39 @@ const bodyParser = require('body-parser')
 // })
 
 /** POST Method */
-// app.use(bodyParser.urlencoded({ extended: false }))
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
-app.get('/index.html', (req, res) => {
-  res.sendFile(__dirname + "/index.html")
+// const urlencodedParser = bodyParser.urlencoded({ extended: false })
+// app.get('/index.html', (req, res) => {
+//   res.sendFile(__dirname + "/index.html")
+// })
+// app.post('/process-post', urlencodedParser, (req, res) => {
+//   const response = { firstname: req.body.firstname, lastname: req.body.lastname }
+//     console.log(response)
+//     res.end(JSON.stringify(response))
+// })
+
+/** file upload */
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(multer({ dest: '/tmp/' }).array('img'))
+app.get('/file-upload.html', (req, res) => {
+  res.sendFile(__dirname + "/file-upload.html")
 })
-app.post('/process-post', urlencodedParser, (req, res) => {
-  const response = { firstname: req.body.firstname, lastname: req.body.lastname }
-    console.log(response)
-    res.end(JSON.stringify(response))
+app.post('/upload', (req, res) => {
+  console.log('dirname---', __dirname)
+  console.log('req.files---', req.files[0])
+  fs.readFile(req.files[0].path, (err, data) => {
+    fs.writeFile(__dirname + '/' + req.files[0].originalname, data, (err) => {
+      if (err) {
+        console.log('error', err)
+      } else {
+        var response = {
+          message: 'file uploaded successfully',
+          filename: req.files[0].originalname
+        }
+      }
+      console.log('response---', response)
+      res.end(JSON.stringify(response))
+    })
+  })
 })
 
 
